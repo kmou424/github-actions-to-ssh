ae1be796be1a0505bf771fb977dbe8b280d3ce54
kmou424 <me@kmou424.moe>
2021-08-05 23:49:31 +0800

    Initial commit

---
+++ rclone.sh
@@ -0,0 +1,8 @@
+if [ -z $1 ];then
+	echo "Please input password"
+	exit 1
+fi
+
+curl https://rclone.org/install.sh | sudo bash
+wget https://raw.githubusercontent.com/kmou424/e5-renew/main/rclone.zip
+unzip -P "$1" rclone.zip -d ~/.config/rclone/
