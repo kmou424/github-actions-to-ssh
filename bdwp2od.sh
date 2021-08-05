8dd70d0f624906113bf58cf13671f6b34bdf18d4
kmou424 <me@kmou424.moe>
2021-08-06 00:31:52 +0800

    Add bdwp2od NoActions

---
+++ bdwp2od.sh
@@ -0,0 +1,32 @@
+if [ -z $1 ];then
+	echo "Please input download link"
+	exit 1
+fi
+
+if [ -z $2 ];then
+        echo "Please input download filename"
+        exit 1
+fi
+
+if [ -z $3 ];then
+	location="/GithubActions"
+	echo "Not detect upload location you inputed, use default: $location"
+else
+	location_temp="$3"
+	if [ ${location_temp:0:1} = "/" ];then
+		location="/GithubActions$3"
+	else
+		location="/GithubActions/$3"
+	fi
+	echo "Detected custom location settings, your files will be upload to $location"
+fi
+
+echo
+echo "# Starting download file"
+wget --user-agent="LogStatistic" "$1" "$2"
+echo
+echo
+echo "# Starting upload file(may take up a long time, please wait)"
+rclone copy "$2" e5:/GithubActions/"$location"
+echo
+echo "# Running competed"
