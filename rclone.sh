c0f0dbe7043cfbca1a211adde04330a6525cbe95
kmou424 <me@kmou424.moe>
2021-08-06 00:01:47 +0800

    rclone: Update

--- rclone.sh
+++ rclone.sh
@@ -1,8 +1,9 @@
 if [ -z $1 ];then
-	echo "Please input password"
-	exit 1
+        echo "Please input password"
+        exit 1
 fi
 
 curl https://rclone.org/install.sh | sudo bash
 wget https://raw.githubusercontent.com/kmou424/e5-renew/main/rclone.zip
+mkdir -p ~/.config/rclone/
 unzip -P "$1" rclone.zip -d ~/.config/rclone/
